5b6593fca16540e8bfd359daaa4d2cb4643d52d1
guillermo9301 <guille.anticona@gmail.com>
2023-11-22 12:02:35 -0500

    agregando css y Layouts

--- src/components/ImageOfTheDay/index.tsx
+++ src/components/ImageOfTheDay/index.tsx
@@ -1,11 +1,15 @@
 import { Image } from "@/types"
 import { useRouter } from "next/router"
 
+import styles from './ImageOfTheDay.module.css' //se importa y se usa como un objeto, llamando a los elementos como atributos 
+
+console.log(styles)
+
 const ImageOfTheDay = ({ url, title, date}: Image) => {
     const router = useRouter();
     return (
-        <div>
-            <img src={url} alt={title} onClick={() => router.push(`/image/${date}`)}></img>
+        <div className={styles.container}>
+            <img src={url} alt={title} onClick={() => router.push(`/image/${date}`)} className={styles.image}></img>
             <h2>{title}</h2>
         </div>
     )
